eef105be4421efef4efe79c47d65dbbf04ab2b71
Na Ilseong <ilseongdev@gmail.com>
2022-12-29 23:42:09 +0900

    feat: 배치 모듈

---
+++ batchgi-batch/src/test/java/com/nailseong/BatchgiApplicationTest.java
@@ -0,0 +1,14 @@
+package com.nailseong;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchgiApplicationTest {
+
+    @DisplayName("애플리케이션 부팅 테스트")
+    @Test
+    void applicationBootingTest() {
+    }
+}
