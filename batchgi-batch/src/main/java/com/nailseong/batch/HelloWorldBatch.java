4835774f9b91ac0f2a538d2097e501dfbedd923d
Na Ilseong <ilseongdev@gmail.com>
2022-12-30 01:38:38 +0900

    feat: validator & JobParameter

--- batchgi-batch/src/main/java/com/nailseong/batch/HelloWorldBatch.java
+++ batchgi-batch/src/main/java/com/nailseong/batch/HelloWorldBatch.java
@@ -2,13 +2,16 @@ package com.nailseong.batch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,26 +19,59 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class HelloWorldBatch {
 
+    private static final String NAME = "name";
+    
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
-                .start(helloWorldStep())
+                .incrementer(new RunIdIncrementer())
+                .start(greetingStep())
+                .next(helloWorldStep())
+                .validator(validator())
                 .build();
+    }
+
+    @Bean
+    public Step greetingStep() {
+        return stepBuilderFactory.get("greetingStep")
+                .tasklet(greetingTasklet(null))
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet greetingTasklet(@Value("#{jobParameters[name]}") String name) {
+        return (contribution, chunkContext) -> {
+            System.out.println(name + " 안녕~~");
+            return RepeatStatus.FINISHED;
+        };
     }
 
     @Bean
     public Step helloWorldStep() {
         return stepBuilderFactory.get("helloWorldStep")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
-                            throws Exception {
-                        System.out.println("배치기!!!!!!");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(batchgiTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet batchgiTasklet() {
+        return (contribution, chunkContext) -> {
+            String name = (String) chunkContext.getStepContext()
+                    .getJobParameters()
+                    .get("name");
+            System.out.println(name + "의 배치기!!!");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public JobParametersValidator validator() {
+        DefaultJobParametersValidator validator = new DefaultJobParametersValidator();
+        validator.setRequiredKeys(new String[]{NAME});
+        return validator;
     }
 }
